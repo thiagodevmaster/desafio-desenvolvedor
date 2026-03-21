bee6dae4b551876ff64bacba2068762d3fcfa169
Thiago Dantas <thiagodantas.dev@gmail.com>
2026-03-21 19:30:21 -0300

    TEST: implement feature tests for login and logout authentication flows

--- trustflow/routes/api.php
+++ trustflow/routes/api.php
@@ -5,5 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest:sanctum')->prefix('/v1')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.user');
+});
+
+Route::middleware('auth:sanctum')->prefix('/v1')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.user-logout');
 });
